346368ed69f1898f612db8c5b439c76388f23f44
Liubov.V.B <griark0611@gmail.com>
2022-11-16 19:51:11 +0300

    add Ex005_Array

--- Ex003/Program.cs
+++ Ex003/Program.cs
@@ -1,11 +1,40 @@
-﻿Console.WriteLine("Введите имя");
-string a = Console.ReadLine();
-if (a.ToLower() == "маша") 
+﻿Console.Clear();
+int xa = 40, ya = 1,
+    xb = 1, yb = 25,
+    xc = 80, yc = 25;
+
+Console.SetCursorPosition (xa, ya);
+Console.WriteLine("+");   
+
+Console.SetCursorPosition (xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition (xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = ya;
+int Count = 0;
+
+while (Count < 10) 
 {
-    Console.WriteLine("Ура! Это же МАША!");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(a);
+    int point = new Random().Next(0, 3);
+    
+    if (point == 0)
+    {
+    x = (x+xa)/2;
+    y = (y+ya)/2;
+    }
+    if (point == 1)
+    {
+    x = (x+xb)/2;
+    y = (y+yb)/2;
+    }
+    if (point == 2)
+    {
+    x = (x+xc)/2;
+    y = (y+yc)/2;
+    }
+    Console.SetCursorPosition (x, y);
+    Console.WriteLine("+");
+    Count++;
 }
